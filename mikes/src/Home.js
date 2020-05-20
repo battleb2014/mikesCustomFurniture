a00eee4a4e4edc41c83532032e1bc7db0e66ff69
Brandon May <49825791+battleb2014@users.noreply.github.com>
2020-05-20 13:13:36 -0400

    local images added

--- mikes/src/Home.js
+++ mikes/src/Home.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Home = () => {
 	return (
 		<div className="home">
-			<h2>Welcome to Mike's.  We take our decades of experience and passion for woodworking, along with the highest quality materials to deliver the best products.</h2><br />
+			<h2>Welcome to Mike's.  We take our decades of experience and passion for woodworking, <br /> along with the highest quality materials to deliver the best products.</h2><br /><br /><br />
+			<img src='./images/Home/drill.jpg' alt='drill' /><br /><br />
+			<h2>We can make anything out of wood.  Our more popular products are cornhole, wooden flags, <br /> end tables and end stands with storage and cup holder.  So browse our gallery and shop and <br /> if what you want
+					isn't there don't hesitate to send us an email with detailed information of what you're <br /> looking for and we'll get back with you quickly.</h2>
 		</div>
 	)
 }
